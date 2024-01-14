ce9d0a6ff94cc71a5ffbee53944989b8d4f5bdbb
ayurkiv <yurkiv.andrew@gmail.com>
2024-01-14 03:18:24 +0200

    DbContext

---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions;
+using Domain.Models;
+
+namespace Infrastructure.Repositories;
+
+public class PostRepository : IPostRepository
+{
+    public readonly ApplicationDbContext _context;
+
+    public PostRepository( ApplicationDbContext context)
+    {
+        this._context = context;
+    }
+
+    public Task<ICollection<Post>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> GetWithComments(int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> Create(Post postData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> Update(string updatedContent, int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> Delete(int postId)
+    {
+        throw new NotImplementedException();
+    }
+}
